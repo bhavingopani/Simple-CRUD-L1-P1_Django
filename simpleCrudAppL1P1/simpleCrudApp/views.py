be74c531f7f06649b41c6f6efcc9273e67712abe
coder7874 <gopani7874@gmail.com>
2022-03-29 18:56:22 +0530

    Simple Crud App Level1 Project1 Continues..
    Applied Auth, did the listing part, created models for listing, user listing, retieved data to views and templates. Some parts are not working - yet to fix those

--- simpleCrudAppL1P1/simpleCrudApp/views.py
+++ simpleCrudAppL1P1/simpleCrudApp/views.py
@@ -1,19 +1,29 @@
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from simpleCrudApp.models import CreateUser
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 def index(request):
-    data = CreateUser.objects.all()
-    START WITH ALL THE THINGS like --- PRINTING data and list it on in the templates.
-    GET THE DATA FROM THE DB And LIST IT.
-    WORK in SPLIT WINDOW 
-    print(data)
+    users = CreateUser.objects.all()  #you can directly check this in shell -- what it prints -- with python3 manage.py shell
+    # print("email", data)
+    # GET THE DATA FROM THE DB And LIST IT.
+    # WORK in SPLIT WINDOW 
+    # print(data)
       
-    return render(request, 'home.html') #the home page will list all the details of the user
+    return render(request, 'home.html',{'users':users}) #the home page will list all the details of the user
 
 
 def loginUser(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/") #redirecting to home page if the user is correct
+        else:
+            return render(request, 'loginUser.html')
     return render(request, 'loginUser.html')
 
 
@@ -34,7 +44,9 @@ def createUser(request):
     return render(request, "createUser.html")
 
 
-
+def logoutUser(request):
+    logout(request)
+    return redirect("/loginUser.html")  #here we are redirecting user to home page or index.html when they logout
 
 
 # def logoutUser(request):
